361790e29739db5a4557407f2fa28b5132c31dff
Han Keun Woo <flatfish@paran.com>
2012-10-24 13:25:53 +0900

    ver1

---
+++ src/com/example/game_dice/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.game_dice;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //button
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
